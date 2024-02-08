de0eb570cb67a05a616226913f9995cd18daa1f8
KeylaConde <keylacondesa@gmail.com>
2024-02-08 15:00:00 -0500

    I added a images carousel

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const menu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const enter = document.querySelector('.entrar');
+const loadingMessage = document.getElementById('loadingMessage');
 
 menu.addEventListener('click', toggleMobileMenu);
+enter.addEventListener('click', redirect);
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+}
+
+function redirect() {
+    loadingMessage.style.display = 'block';
+    setTimeout(function () {
+        window.location.href = './productos.html';
+    }, 1000); 
 }
