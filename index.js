843856bce48643de0b6a3a3ebb0302e8e6e6f548
KeylaConde <keylacondesa@gmail.com>
2024-03-06 22:03:52 -0500

    My page is ready, my shopping cart has a function

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const menu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const enter = document.querySelector('.entrar');
 const loadingMessage = document.getElementById('loadingMessage');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const iconFlecha = document.querySelector('.flecha');
 
 menu.addEventListener('click', toggleMobileMenu);
 enter.addEventListener('click', redirect);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+iconFlecha.addEventListener('click', closeCarritoAside);
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
@@ -15,4 +20,12 @@ function redirect() {
     setTimeout(function () {
         window.location.href = './productos.html';
     }, 1000); 
+}
+
+function toggleCarritoAside() {
+    shoppingCartContainer.classList.toggle('inactive')
+}
+
+function closeCarritoAside() {
+    shoppingCartContainer.classList.add('inactive');
 }
